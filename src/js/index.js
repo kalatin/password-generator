86b97f801c932901644bf5a1a0259d569572463a
Ruslan Pitsik <rs.pitsik@gmail.com>
2023-04-05 00:30:56 +0300

    add add a good password generator

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,50 @@
 class PassGen {
 	constructor() {
 		this.password = '';
-		this.passCount = 50;
+		this.passCount = 12;
 		this.symbols = {
 			bigLetters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
 			smallLetters: 'abcdefghijklmnopqrstuvwxyz'.split(''),
 			numbers: '0123456789'.split(''),
-			specSymbols: '!@#$%^&*()'.split(''),
+			specSymbols: '!@#$%^&*_~-'.split(''),
 		};
 
 		for (let i = 0; i < this.passCount; i++) {
 			let whatKey = Math.floor(Math.random() * Object.keys(this.symbols).length);
 			let whatObj = this.symbols[Object.keys(this.symbols)[whatKey]];
 			let whatSymbol = whatObj[Math.floor(Math.random() * Object.keys(whatObj).length)];
-
+			// console.log(
+			// 	whatObj.includes(whatSymbol),
+			// 	whatObj.includes(this.password.at(-1)),
+			// 	whatObj.includes(this.password.at(-2)),
+			// 	'---',
+			// 	whatSymbol,
+			// 	this.password
+			// );
 			if (this.password.length < 1) {
 				this.password += whatSymbol;
 			} else if (this.password.length >= 1) {
-				let addedLetter = this.password + whatSymbol;
-
-				if (this.password.at(-1) === whatSymbol || this.password.includes(addedLetter.at(-1))) {
+				if (
+					this.password.at(-1) === whatSymbol ||
+					this.password.includes(whatSymbol) ||
+					(whatObj.includes(whatSymbol) &&
+						whatObj.includes(this.password.at(-1)) &&
+						whatObj.includes(this.password.at(-2)))
+				) {
 					i--;
 					continue;
 				} else {
 					this.password += whatSymbol;
 				}
 			}
+
+			// console.log(whatKey, whatObj, whatSymbol);
 		}
 	}
 }
 
 let ex1 = new PassGen();
+// console.log(ex1);
+// console.log(ex1.password.length);
 console.log(ex1.password);
-console.log(ex1.password.length);
-console.log(new Set(ex1.password).size);
+// console.log(new Set(ex1.password).size);
