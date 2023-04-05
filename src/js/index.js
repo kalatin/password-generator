0bb2640da8f6e97ab012386440521b6d1d679431
Ruslan Pitsik <rs.pitsik@gmail.com>
2023-04-05 19:31:56 +0300

    add range input text

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
+import { inputRange } from './inputRange.js';
+
+inputRange();
+
 class PassGen {
 	constructor() {
 		this.password = '';
-		this.passCount = 12;
+		this.passCount = 10;
 		this.symbols = {
 			bigLetters: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
 			smallLetters: 'abcdefghijklmnopqrstuvwxyz'.split(''),
@@ -13,14 +17,6 @@ class PassGen {
 			let whatKey = Math.floor(Math.random() * Object.keys(this.symbols).length);
 			let whatObj = this.symbols[Object.keys(this.symbols)[whatKey]];
 			let whatSymbol = whatObj[Math.floor(Math.random() * Object.keys(whatObj).length)];
-			// console.log(
-			// 	whatObj.includes(whatSymbol),
-			// 	whatObj.includes(this.password.at(-1)),
-			// 	whatObj.includes(this.password.at(-2)),
-			// 	'---',
-			// 	whatSymbol,
-			// 	this.password
-			// );
 			if (this.password.length < 1) {
 				this.password += whatSymbol;
 			} else if (this.password.length >= 1) {
@@ -37,14 +33,16 @@ class PassGen {
 					this.password += whatSymbol;
 				}
 			}
-
-			// console.log(whatKey, whatObj, whatSymbol);
 		}
 	}
 }
 
 let ex1 = new PassGen();
-// console.log(ex1);
-// console.log(ex1.password.length);
-console.log(ex1.password);
-// console.log(new Set(ex1.password).size);
+document.write(ex1.password);
+
+function rangeProcent() {
+	let max = 25;
+	let min = 12;
+	let our = 13;
+	console.log();
+}
---
+++ src/js/inputRange.js
@@ -0,0 +1,26 @@
+export function inputRange() {
+	let inputContainer = document.querySelector('.password__range');
+	let inputRangeValue = inputContainer.querySelector('output');
+	let inputRange = inputContainer.querySelector('input');
+	setText();
+	inputRange.addEventListener('input', setText);
+
+	function setText() {
+		let value = inputRange.value;
+		let min = inputRange.min;
+		let max = inputRange.max;
+
+		inputRangeValue.textContent = value;
+		inputRangeValue.style.left = `${((value - min) / (max - min)) * 100}%`;
+		inputRangeValue.style.transform = `translate(-${((value - min) / (max - min)) * 100}%,0)`;
+
+		if (value < (+max + +min) / 2) {
+			console.log(Math.floor((+max + +min) / 2));
+			inputRangeValue.style.paddingLeft = '3px';
+		} else if (value >= Math.ceil((+max + +min) / 2)) {
+			console.log();
+			inputRangeValue.style.paddingLeft = '0px';
+			inputRangeValue.style.paddingRight = '2px';
+		}
+	}
+}
